e73dd9719ad2662cd2627619ac30d1ccea6ff89d
Léonard Wojcik <leonardw@tutanota.com>
2022-09-03 11:06:02 +0200

    feat: add personal custom plugin - Test if personal custom plugin is properly imported.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,17 @@
 // Routing
 import { Link } from 'react-router-dom';
 
-function Home() {
+// Libraries
+import { SelectMenu } from '@leoncik/p14-hrnet-plugin';
 
+function Home() {
     return (
-      <div className="App">
-        <p>Home page.</p>
-        <Link to="/employee-list">To the employee list page</Link>
-      </div>
-    )
-  }
-  
-  export default Home
-  
+        <div className="App">
+            <p>Home page.</p>
+            <SelectMenu label="I am a test :-)" />
+            <Link to="/employee-list">To the employee list page</Link>
+        </div>
+    );
+}
+
+export default Home;
