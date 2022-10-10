06d7224ffad7a6aa991b2931971c50a946b1337f
Léonard Wojcik <leonardw@tutanota.com>
2022-10-10 22:47:29 +0200

    feat: add logo

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,18 +1,22 @@
 // Routing
 import { NavLink } from 'react-router-dom';
 
+// Assets
+import logo from '../../../assets/WealthHealth-logo.jpg';
+
 function Header() {
     return (
-        <nav className="main-nav flex justify-around p-4 bg-white">
+        <nav className="main-nav flex items-center justify-around p-4 bg-white">
             <NavLink
-                className="main-nav-logo"
+                className="main-nav-logo flex items-center gap-4"
                 to="/"
                 style={({ isActive }) => ({
                     textDecoration: isActive ? 'underline' : 'none',
-                    textDecorationColor: isActive ? 'rgb(34 197 94)' : 'none',
+                    textDecorationColor: isActive ? '#93ad18' : 'none',
                 })}
             >
-                <h1 className="text-green-500 hover:opacity-60 active:opacity-40">
+                <img className="w-8 peer" src={logo} alt="HRnet logo." />
+                <h1 className="text-[#93ad18] hover:opacity-60 active:opacity-40 h-fit peer-hover:opacity-60 peer-active:opacity-40">
                     HRnet
                 </h1>
             </NavLink>
